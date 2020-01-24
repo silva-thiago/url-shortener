610153dbf3df8f8e362554a13eb2b2883d4e15cb
thiagosilva <thiago.silva@imd.ufrn.br>
2020-01-24 19:18:32 -0300

    Gerenciamento dos formulários de login e registro desenvolvido

---
+++ accounts/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, get_user_model, login, logout
+from .forms import UserSignInForm, UserSignUpForm
+
+
+def signin_view(request):
+    next = request.GET.get('next')
+    form = UserSignInForm(request.POST or None)
+    if form.is_valid():
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password')
+        user = authenticate(username=username, password=password)
+        login(request, user)
+        if next:
+            return redirect(next)
+        return redirect('/')
+
+    context = {'form': form, }
+    return render(request, 'signin.html', context)
+
+
+def signup_view(request):
+    next = request.GET.get('next')
+    form = UserSignUpForm(request.POST or None)
+    if form.is_valid():
+        user = form.save(commit=False)
+        password = form.cleaned_data.get('password')
+        user.set_password(password)
+        user.save()
+        new_user = authenticate(username=user.username, password=password)
+        login(request, new_user)
+        if next:
+            return redirect(next)
+        return redirect('/')
+
+    context = {'form': form, }
+    return render(request, 'signup.html', context)
+
+
+def signout_view(request):
+    logout(request)
+    return redirect('/')
