7241461f46c3ea323631eb3fe320632180cc0bf6
thiagosilva <thiago.silva@imd.ufrn.br>
2020-01-27 20:14:20 -0300

    Banco de dados padrão renomeado

--- admin/settings.py
+++ admin/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'db_urlshortener',
+        'NAME': 'db_shortener_url',
         'USER': 'root',
         'PASSWORD': '',
         'HOST': '127.0.0.1',
